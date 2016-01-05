d41ea9ca76cc1b386c72dabe5175d42f423e47ea
Eric Gentry <egentry@ucsc.edu>
2016-01-04 20:32:51 -0800

    Added initial files
    I expect only minor changes from here on out.
    But we'll see if that actually happens.

---
+++ day4/rv_helper.py
@@ -0,0 +1,45 @@
+
+import numpy as np 
+import scipy
+
+def time_to_theta(t, t_0, P, e):
+	"""
+	Converts time into true anomaly (theta in our conventions)
+
+	This requires numerical root finding (see scipy.optimize.newton)
+
+	Parameters
+	----------
+	t : float
+		Current time [seconds]
+	t_0 : float
+		reference time [seconds]
+	P : float
+		Orbital period [seconds]
+	e : float
+		Orbital eccentricity
+
+	Returns
+	-------
+	theta : float
+		Mean anomaly
+	"""
+
+	# first solve for eccentric anomaly (eta)
+
+	phase = 2*np.pi*(t-t_0)/P %(2*np.pi)
+
+	phase = np.array(phase, ndmin=1) # otherwise iterating below fails
+	etas = np.empty_like(phase)
+	for i, p in enumerate(phase):
+
+		def _solve_this(eta):
+			return eta - e*np.sin(eta) - p
+
+		etas[i] = scipy.optimize.brentq(_solve_this, 0, 2*np.pi+.00001)
+
+	# now solve for true anomaly
+	theta = 2 * np.arctan(((1+e)/(1-e))**.5 * np.tan(etas/2.))
+
+	return theta
+
